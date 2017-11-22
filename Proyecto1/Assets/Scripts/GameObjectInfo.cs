c5ff26ead44b1c2b33c04c31838148d1cad268ba
AzizLabib2017 <aziz.labib2014@gmail.com>
2017-11-22 20:42:52 +0100

    proyectos subidos desde clase d.videojuegos

---
+++ Proyecto1/Assets/Scripts/GameObjectInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectInfo : MonoBehaviour {
+
+    public int duracion = 5;
+	// Use this for initialization
+	void Start () {
+        string goName=gameObject.name;
+        Debug.Log("EL GameObject "+goName+" se destruye en 5 segundos.");
+        Destroy(gameObject,duracion);
+        
+
+    }
+    private void OnDestroy()
+    {
+        Debug.Log("EL GameObject Destruido.");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Proyecto1/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour {
+
+    public string frase = "Eyyyyyyyy";
+    public int num = 5;
+    public float decimales = 2.4f;
+    public bool booleano=true;
+    public PlayMode pmode;
+    public Color color ;
+
+     void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("este es un mensaje informativo");
+        Debug.LogWarning("este es un mensaje warning");
+        Debug.LogError("este es un mensaje Error");
+    }
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+    }
+    void OnDestroy()
+    {
+        Debug.Log("OnDistroy");
+    }
+    //// Update is called once per frame
+    //void Update () {
+    //    Debug.Log("updata cada frame");
+    //}
+	 
+    
+}
